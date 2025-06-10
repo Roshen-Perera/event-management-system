4266b8b5d14d274feba605e0fe3d4f9a8a25e3a4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-06-10 17:58:15 +0530

    Entered model data

---
+++ model/Event.ts
@@ -0,0 +1,10 @@
+export default class Event {
+    name!: string;
+    description!: string;
+    date!: Date;
+    location!: string;
+    createdBy!: string;
+    capacity!: number;
+    remaining_capacity!: number;
+    tags !: string[];
+}
