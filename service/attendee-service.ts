2ea222eef426fbbeafe09b3e711bc7975aa7e954
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-06-11 19:25:04 +0530

    Refactor addAttendee function to use dynamic attendee data for name, email, and registered events

--- service/attendee-service.ts
+++ service/attendee-service.ts
@@ -8,12 +8,11 @@ export async function addAttendee(attendee: Attendee){
     try{
         await prisma.attendee.create({
             data: {
-                name: "Jane Doe",
-                email: "jane@example.com",
+                name: attendee.name,
+                email: attendee.email,
                 registered_events: {
                     create: [
-                        { eventId: "55ee2ff5-5865-4059-8e6b-0075b0208c17" },
-                        { eventId: "5c4d39e7-fc8f-4dca-b8d3-4980d2f9f0b9" }
+                        { eventId: attendee.registered_events }
                     ]
                 }
             }
