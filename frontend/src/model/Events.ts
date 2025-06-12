6b89e9db25c0c00cbcfe332b391294ee1d3c7101
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-06-12 19:53:55 +0530

    Add Attendee and Events model classes

---
+++ frontend/src/model/Events.ts
@@ -0,0 +1,10 @@
+export default class Events {
+    name!: string;
+    description!: string;
+    date!: Date;
+    location!: string;
+    createdBy!: string;
+    capacity!: number;
+    remaining_capacity!: number;
+    tags !: string;
+}
