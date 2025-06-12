99c8baa566b082f1c9310d134b803b10a661ff04
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-06-12 20:24:50 +0530

    Implement event fetching in EventList component using Redux

--- frontend/src/pages/EventList.tsx
+++ frontend/src/pages/EventList.tsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import type Events from "../model/Events";
+import { getEvent } from "../slices/EventSlice";
+import { useDispatch, useSelector } from "react-redux";
+import type { AppDispatch } from "../store/Store";
 
 const EventList = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const events = useSelector((state: { event: Events[] }) => state.event);
+  
+      useEffect(() => {
+      if (events.length === 0) dispatch(getEvent());
+    }, [dispatch, events.length]);
+  
   return (
     <div className="pt-4 p-4">
       <table className="table">
